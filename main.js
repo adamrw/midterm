90349950987adb79644dbf158d3951ce9955880e
Adam Weiner <adamwineguy@gmail.com>
2014-10-09 10:04:25 -0600

    fixed what I broke yesterday :)

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(document).on('ready', function() {
 
    $('#firstForm').on('submit', function(e){
    		e.preventDefault();
+   		$('#posAlert').find('.closealert').click();
+   		$('#negAlert').find('.closealertneg').click();
    		var getVal = $('#InputCompany').val();
    		console.log('test', getVal);
    		var foundCompany=null;
@@ -20,14 +22,14 @@ $(document).on('ready', function() {
 //If company is not found, display a different alert telling user no data is found
 
    		if(foundCompany !== null){
-			$('#posAlert').html('<div class="alert alert-success alert-dismissable" role="alert">' + foundCompany.name + ' is owned by ' + foundCompany.owners + ' and is associated with ' + foundCompany.associates + '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' + '</div>');
+			$('#posAlert').html('<div class="alert alert-success alert-dismissable" role="alert">' + foundCompany.name + ' is owned by ' + foundCompany.owners + ' and is associated with ' + foundCompany.associates + '<button type="button" class="close" data-dismiss="alert"><span class="closealert" aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' + '</div>');
 			// alert(companies[i].name + ' is owned by ' + companies[i].owners + ' and is associated with ' + companies[i].associates);
 			// foundCompany = Companies.splice(i, 1);
 		
 		// return companies[i].associates;
 	   	 }
    		else  {
-   			$('#negAlert').html('<div class="alert alert-danger alert-dismissable" role="alert">' + "I'm sorry, we didn't find any results for that company" + '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' + '</div>');
+   			$('#negAlert').html('<div class="alert alert-danger alert-dismissable" role="alert">' + "I'm sorry, we didn't find any results for that company" + '<button type="button" class="close" data-dismiss="alert"><span class="closealertneg" aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' + '</div>');
    		}
    	});
 
@@ -42,15 +44,7 @@ $(document).on('ready', function() {
 		this.owners = owners;
 		this.associates = associates;
 	
-	var wall = new Company('Wall Mart', ['Bob'], []);
-	var puri = new Company('Purina', ['Joe'], []);
-	var chip = new Company('Chipotle', ['Gina'], []);
-	var pepsi = new Company('Pepsi', ['John'], []);
-	var pizz = new Company('Pizzeria Locale', ['Pete'], [chip.name]);
-	var dog = new Company('Dog Foodz', ['Stephanie'], [puri.name, wall.name]);
-	var sand = new Company('Quiznos', ['Meghan'], [chip.name, pepsi.name, puri.name]);
-		// console.log(chip);
-	var companies=[wall, puri, chip, pepsi, pizz, dog, sand];
+	
 
 
 //Allow user to add information about companies
@@ -69,7 +63,14 @@ $(document).on('ready', function() {
 		
 		// }
 	
-
-
+var wall = new Company('Wall Mart', ['Bob'], []);
+	var puri = new Company('Purina', ['Joe'], []);
+	var chip = new Company('Chipotle', ['Gina'], []);
+	var pepsi = new Company('Pepsi', ['John'], []);
+	var pizz = new Company('Pizzeria Locale', ['Pete'], [chip.name]);
+	var dog = new Company('Dog Foodz', ['Stephanie'], [puri.name, wall.name]);
+	var sand = new Company('Quiznos', ['Meghan'], [chip.name, pepsi.name, puri.name]);
+		// console.log(chip);
+	var companies=[wall, puri, chip, pepsi, pizz, dog, sand];
 });
 
